bf779d5ef6262a905c5882cc6834cbee0e6fc12d
Princess-of-Sleeping <29831892+Princess-of-Sleeping@users.noreply.github.com>
2022-11-29 22:39:44 +0900

    Add more headers

---
+++ header/kernel/kernel/modulemgr.h
@@ -0,0 +1,21 @@
+
+#ifndef _PSP2_KERNEL_MODULEMGR_H_
+#define _PSP2_KERNEL_MODULEMGR_H_
+
+#include <shared/types.h>
+
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+
+#define SCE_KERNEL_START_SUCCESS (0)
+
+
+#ifdef __cplusplus
+}
+#endif
+
+
+#endif /* _PSP2_KERNEL_MODULEMGR_H_ */
