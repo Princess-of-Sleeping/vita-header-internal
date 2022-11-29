2a18f8867f900dea8c329991374dd1eda16a379c
Princess-of-Sleeping <29831892+Princess-of-Sleeping@users.noreply.github.com>
2022-11-29 21:52:04 +0900

    Create debug.h

---
+++ header/kernel/kernel/debug.h
@@ -0,0 +1,21 @@
+
+#ifndef _PSP2_KERNEL_DEBUG_H_
+#define _PSP2_KERNEL_DEBUG_H_
+
+#include <shared/types.h>
+
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+
+int sceKernelPrintf(const char *fmt, ...);
+
+
+#ifdef __cplusplus
+}
+#endif
+
+
+#endif /* _PSP2_KERNEL_DEBUG_H_ */
