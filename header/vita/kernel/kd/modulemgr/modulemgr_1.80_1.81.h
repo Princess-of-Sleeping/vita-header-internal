fd06467a43f7826f5207c0c5cd5785a408b66b9b
Princess-of-Sleeping <princess.of.sleeping.0x209@gmail.com>
2023-03-09 21:40:38 +0900

    Update modulemgr_1.80_1.81.h

--- header/vita/kernel/kd/modulemgr/modulemgr_1.80_1.81.h
+++ header/vita/kernel/kd/modulemgr/modulemgr_1.80_1.81.h
@@ -1,5 +1,70 @@
 
+#ifndef _PSP2_KERNEL_MODULEMGR_INTERNAL_H_
+#define _PSP2_KERNEL_MODULEMGR_INTERNAL_H_
+
+
+#include <psp2kern/types.h>
+
+
+
+typedef uint16_t Elf32_Half;
+typedef uint32_t Elf32_Word;
+typedef	int32_t  Elf32_Sword;
+typedef uint64_t Elf32_Xword;
+typedef	int64_t  Elf32_Sxword;
+typedef uint32_t Elf32_Addr;
+typedef uint32_t Elf32_Off;
+
+#define EI_NIDENT (16)
+
+typedef struct
+{
+  unsigned char	e_ident[EI_NIDENT];	/* Magic number and other info */
+  Elf32_Half	e_type;			/* Object file type */
+  Elf32_Half	e_machine;		/* Architecture */
+  Elf32_Word	e_version;		/* Object file version */
+  Elf32_Addr	e_entry;		/* Entry point virtual address */
+  Elf32_Off	e_phoff;		/* Program header table file offset */
+  Elf32_Off	e_shoff;		/* Section header table file offset */
+  Elf32_Word	e_flags;		/* Processor-specific flags */
+  Elf32_Half	e_ehsize;		/* ELF header size in bytes */
+  Elf32_Half	e_phentsize;		/* Program header table entry size */
+  Elf32_Half	e_phnum;		/* Program header table entry count */
+  Elf32_Half	e_shentsize;		/* Section header table entry size */
+  Elf32_Half	e_shnum;		/* Section header table entry count */
+  Elf32_Half	e_shstrndx;		/* Section header string table index */
+} Elf32_Ehdr;
+
+typedef struct
+{
+  Elf32_Word	p_type;			/* Segment type */
+  Elf32_Off	p_offset;		/* Segment file offset */
+  Elf32_Addr	p_vaddr;		/* Segment virtual address */
+  Elf32_Addr	p_paddr;		/* Segment physical address */
+  Elf32_Word	p_filesz;		/* Segment size in file */
+  Elf32_Word	p_memsz;		/* Segment size in memory */
+  Elf32_Word	p_flags;		/* Segment flags */
+  Elf32_Word	p_align;		/* Segment alignment */
+} Elf32_Phdr;
+
+
 typedef struct SceModuleCB SceModuleCB;
+
+
+typedef struct SceModuleExport {
+	uint8_t size; // 0x20
+	uint8_t auxattr;
+	uint16_t version;
+	uint16_t attr; // 0x4000:user export
+	uint16_t entry_num_function;
+	uint16_t entry_num_variable;
+	uint16_t data_0x0A; // unused?
+	uint32_t data_0x0C; // unused?
+	uint32_t libnid;
+	const char *libname;
+	uint32_t *table_nid;
+	void    **table_entry;
+} SceModuleExport;
 
 typedef struct SceModuleLibEnt { // size is 0x60-bytes
 	SceUInt32 data_0x00;
@@ -98,3 +163,6 @@ typedef struct SceKernelLibraryDB { // size is 0x3C-bytes
 	SceUInt32 data_0x34;
 	SceUInt32 data_0x38;
 } SceKernelLibraryDB;
+
+
+#endif /* _PSP2_KERNEL_MODULEMGR_INTERNAL_H_ */
