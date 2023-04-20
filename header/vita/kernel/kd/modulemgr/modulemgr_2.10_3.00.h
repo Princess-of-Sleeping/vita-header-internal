145e2e49a32a91bc5af23733ea37e34dc71a2304
Princess-of-Sleeping <29831892+Princess-of-Sleeping@users.noreply.github.com>
2023-04-20 21:17:39 +0900

    Update modulemgr_2.10_3.00.h

--- header/vita/kernel/kd/modulemgr/modulemgr_2.10_3.00.h
+++ header/vita/kernel/kd/modulemgr/modulemgr_2.10_3.00.h
@@ -1,6 +1,12 @@
 
+#ifndef _PSP2_KERNEL_MODULEMGR_INTERNAL_H_
+#define _PSP2_KERNEL_MODULEMGR_INTERNAL_H_
 
 
+#include <psp2kern/types.h>
+
+
+/*
 typedef struct SceKernelModulePrivate9 { // size is 0x84-bytes on 3.00
 	SceClass SceUIDModuleClass;
 	SceClass SceUIDLibraryClass;
@@ -9,8 +15,46 @@ typedef struct SceKernelModulePrivate9 { // size is 0x84-bytes on 3.00
 	SceUID ScePsp2BootConfig_modid;
 } SceKernelModulePrivate9;
 
-typedef struct SceModuleImport SceModuleImport;
+*/
+
 typedef struct SceModuleCB SceModuleCB;
+typedef struct SceModuleLibEnt SceModuleLibEnt;
+
+typedef struct SceModuleImportOld { // size is 0x34-bytes
+	SceUInt8 size;
+	SceUInt8 auxattr;
+	uint16_t version;
+	uint16_t flags;
+	uint16_t entry_num_function;
+	uint16_t entry_num_variable;
+	uint16_t entry_num_tls;
+	uint32_t rsvd1;
+	uint32_t libnid;
+	const char *libname;
+	uint32_t rsvd2;
+	uint32_t *table_func_nid;
+	void    **table_function;
+	uint32_t *table_vars_nid;
+	void    **table_variable;
+	uint32_t *table_tls_nid;
+	void    **table_tls;
+} SceModuleImportOld;
+
+typedef struct SceModuleImport { // size is 0x24-bytes
+	SceUInt8 size;
+	SceUInt8 auxattr;
+	uint16_t version;
+	uint16_t flags;
+	uint16_t entry_num_function;
+	uint16_t entry_num_variable;
+	uint16_t data_0x0A; // unused?
+	SceNID libnid;
+	const char *libname;
+	SceNID *table_func_nid;
+	void  **table_function;
+	SceNID *table_vars_nid;
+	void  **table_variable;
+} SceModuleImport;
 
 typedef struct SceModuleExport {
 	uint8_t size; // 0x20
@@ -27,21 +71,19 @@ typedef struct SceModuleExport {
 	void    **table_entry;
 } SceModuleExport;
 
-typedef struct SceModuleSegment { // size is 0x20-bytes on 3.00
-	int flags;
-	int flags2;
-	ScePVoid address;
-	SceUID memblock_id;
-	SceUInt32 unk_0x10;
-	ScePVoid address2;
-	int flags3;
-	SceSize length;
-} SceModuleSegment;
+typedef struct SceModuleClient { // size is 0x18-bytes
+	struct SceModuleClient *next;
+	SceModuleImport *Import;
+	SceModuleImport *Import2;
+	SceModuleLibEnt *LibEnt;
+	SceModuleCB *Module;
+	ScePVoid stubtbl; // Required the dipsw 0xD2. stub pointer + stub nids
+} SceModuleClient;
 
-typedef struct SceModuleLibraryInfo { // size is 0x30-bytes on 3.00
-	struct SceModuleLibraryInfo *next;
-	struct SceModuleLibraryInfo *data_0x04; // maybe
-	SceModuleExport *pExportInfo;
+typedef struct SceModuleLibEnt { // size is 0x30-bytes on 3.00
+	struct SceModuleLibEnt *next;
+	struct SceModuleLibEnt *data_0x04; // maybe
+	SceModuleExport *Export;
 	SceModuleExport *pExportInfo2;
 
 	/*
@@ -56,20 +98,37 @@ typedef struct SceModuleLibraryInfo { // size is 0x30-bytes on 3.00
 	 * Number of times this export was imported into another module
 	 */
 	SceSize number_of_imported;
-	SceModuleImportInfo *pImportedInfo;
+	SceModuleClient *Client;
 	SceUID libid_kernel;
 	SceUID libid_user;
-	SceModuleCB *pModuleInfo;
+	SceModuleCB *Module;
 	int data_0x24; // zero?
 	int data_0x28; // zero?
-} SceModuleLibraryInfo;
+} SceModuleLibEnt;
+
+typedef struct SceModuleSegment { // size is 0x20-bytes on 3.00
+	int flags;
+	int flags2;
+	ScePVoid vaddr;
+	SceUID memblock_id;
+	SceUInt32 unk_0x10;
+	ScePVoid address2;
+	int flags3;
+	SceSize length;
+} SceModuleSegment;
+
+typedef struct _SceModuleCB {
+	struct _SceModuleCB *next;
+	SceModuleCB *Module;
+	int a3;
+} _SceModuleCB;
 
 typedef struct SceModuleCB { // size is 0x114-bytes on 3.00
 	SceUID module_guid;
 	SceUID module_puid;
 	SceUInt16 attr;
 	SceUInt16 version;
-	SceChar8 module_name[0x1C];
+	char module_name[0x1C];
 	SceModuleExport *export_start;
 	SceModuleExport *export_end;
 	SceModuleImport *import_start;
@@ -97,11 +156,12 @@ typedef struct SceModuleCB { // size is 0x114-bytes on 3.00
 	ScePVoid sce_module_start_thread_parameter;
 	ScePVoid sce_module_stop_thread_parameter;
 	ScePVoid unk_0xEC;
-	SceUInt32 unk_0xF0;
+	_SceModuleCB *unk_0xF0;
 	SceUInt16 flags;
 	SceUInt8 state;
 	SceUInt8 pad_0xF7;
-	struct SceModuleCB *prev_module;
+	// struct SceModuleCB *prev_module; // real name
+	struct SceModuleCB *next;
 	SceUID processId;
 	SceUInt32 module_sdk_version;
 	SceUInt32 unk_0x104;
@@ -115,3 +175,21 @@ typedef struct SceUIDModuleObject { // size is 0x120-bytes on 3.00
 	SceModuleCB cb;
 	int data_0x11C;
 } SceUIDModuleObject;
+
+
+typedef struct SceKernelLibraryDB { // size is 0x24-bytes
+	SceUID pid;
+	SceModuleLibEnt *LibEntHead;
+	SceUInt16 LibEntCounter;
+	SceUInt16 LostClientCounter;
+	SceModuleClient *LostClientHead;
+	SceModuleCB *ModuleHead;
+	SceUID ModuleId;
+	SceUInt16 ModuleCounter;
+	SceUInt16 flags;
+	void *Addr2ModTbls;
+	SceUInt32 mutex;
+} SceKernelLibraryDB;
+
+
+#endif /* _PSP2_KERNEL_MODULEMGR_INTERNAL_H_ */
